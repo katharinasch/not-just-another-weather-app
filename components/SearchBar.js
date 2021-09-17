05bac7d7ee64a3a154abf4a9ad82e232ab401d4a
katharinasch <ms.schreiber.katharina@gmail..com>
2021-09-17 13:27:17 +0200

    2 separate components, city undefined

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
-export const SearchBar = () => {
-  const [city, setCity] = useState('')
-  const [payload, setPayload] = useState(null)
+export const SearchBar = ({city, setCity}) => {
+
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(true)
   const [cityList, setCityList] = useState(null)
-
 
   const handleOnSearch = (string, cityList) => {
     if(string.length >= 3) {
@@ -30,39 +28,14 @@ export const SearchBar = () => {
   }
 
   const allCities = cityList ? cityList : []
-  console.log('all cities: ', allCities)
-
   const handleOnSelect = (item) => {
     const city = item?.name
-    setCity(city)   
+    setCity(city)
+    console.log(city)  
   }
-
-  const fetchCityData = () => {
-    const options = {
-      method: `POST`,
-    };
-    fetch(`/api/weather?city=${city}`, options)
-    .then((response) => {
-      if(response.ok){
-        return response.json().then(setPayload)
-      }
-        throw new Error('Api is not available') 
-      })
-    .catch(error => {
-      console.error('Error fetching data: ', error)
-      setError(error)
-    })
-    .finally(setLoading(false))
-  }
-
-  console.log(payload)
-
-  const location = payload?.location?.name;
-  const currentTemp = payload?.current?.temp_c;
-
 
   return(
-    <div >
+    <div className="mb-6">
       <label htmlFor="email" className="block text-sm font-medium text-gray-700">
         Search city
       </label>
@@ -84,14 +57,11 @@ export const SearchBar = () => {
           </div>
         </div>
         <div className="col-span-2 mt-1">
-          <button className="mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type='button' onClick={fetchCityData}>
-            Check weather
+          <button className="mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type='button' onClick={handleOnSelect}>
+            Select City
           </button>
         </div>
       </div>
-      <p key={city?.location?.id} className='my-5'>
-        { location ? `Current weather in ${location} is ${currentTemp} degrees ` : 'Please search for city to see current weather'}
-      </p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
-import {SearchBar} from '../components/SearchBar'
-import React from 'react'
+import { SearchBar } from '../components/SearchBar.js'
+import { ForecastButtons } from '../components/ForecastButtons.js'
+import React, { useState } from 'react'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+
 
 export default function Form() {
+  const [city, setCity] = useState('')
+
   return (
     <div className="bg-blue-200">
       <div className="container mx-auto py-36 bg-blue-200">
         <div className="mt-12 px-96">
           <form action="#" method="POST" className="sm:grid-cols-2 sm:gap-x-8">
             
-            <SearchBar />
-            
-            <div className="sm:col-span-2">
-              <p className="block text-sm font-medium text-gray-700">For when are you looking a</p>
-              <button className="mt-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                Today
-              </button>
-            </div>
+            <SearchBar city={city} setCity={setCity} />
+            <ForecastButtons city={city}/>
+
           </form>
       </div>
     </div>
